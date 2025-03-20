67edca72a4f9caf0b72534ac03eebfcf45889833
paulooliveira152012 <paulooliveira152012@gmail.com>
2025-03-19 22:25:34 -0400

    work on profile image/menu

---
+++ frontend/components/components/InteractionBox.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const InteractionBox = () => {
+  return (
+    <View>
+      <Text>InteractionBox</Text>
+    </View>
+  )
+}
+
+export default InteractionBox
